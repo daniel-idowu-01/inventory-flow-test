14088f5aae117a4607834cf656173695d3142ee9
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-25 22:40:14 +0100

    added title and value to dashboard stats comp

--- client/src/components/DashboardComp.jsx
+++ client/src/components/DashboardComp.jsx
@@ -3,9 +3,14 @@ import StatsComp from './StatsComp'
 
 const DashboardComp = () => {
   return (
-    <section className='h-screen bg-red-400'>
-      <div className='absolute left-[25%] bg-blue-500 h-full'>
-        <p>Dashboard</p>
+    <section className='h-screen'>
+      <div className='absolute left-[25%] h-full w-[70%]'>
+        <article className='flex justify-between p-10'>
+          <StatsComp title='Bookings' value='234' percent='+55' />
+          <StatsComp title="Today's users" value='43' percent='+3' />
+          <StatsComp title='Revenue' value='48k' percent='+1' />
+          <StatsComp title='Followers' value='+82' percent='Just updated' />
+        </article>
       </div>
     </section>
   )
--- client/src/components/StatsComp.jsx
+++ client/src/components/StatsComp.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 
-const StatsComp = () => {
+const StatsComp = ({title, value, percent}) => {
   return (
-    <div>
-      StatsComp
-    </div>
+    <article className='p-2 shadow-md border w-52 h-32 rounded-xl'>
+      <section
+        className='flex justify-end text-right '
+      >
+        <div></div>
+
+        <div>
+          <p>{title}</p>
+          <p className='text-xl font-bold'>{value}</p>
+        </div>
+      </section>
+
+      <hr className='w-[70%] mx-auto mt-3' />
+
+      
+      
+    </article>
   )
 }
 
