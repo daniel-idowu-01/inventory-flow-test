1c1d10a83d273d5f811b105af18166c26b2f8436
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-30 23:36:05 +0100

    created login path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 /* #49598a */
 import Sidebar from "./components/Sidebar"
 import DashboardComp from "./components/DashboardComp"
+import SignIn from './pages/SignIn'
 import { DataProvider } from "./context/DataContext"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
@@ -13,6 +14,7 @@ function App() {
           <Sidebar />
           <Routes>
             <Route path="/" element={<DashboardComp />} />
+            <Route path="/login" element={<SignIn />} />
           </Routes>
         </DataProvider>
       </Router>
