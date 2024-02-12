609504bad51b912bfcdc942f54f7409722cf6ecb
daniel-idowu-01 <danielidowu414@gmail.com>
2024-02-12 18:59:09 +0100

    added category json file

--- client/src/pages/CreateProduct.jsx
+++ client/src/pages/CreateProduct.jsx
@@ -14,10 +14,10 @@ const CreateProduct = () => {
   }
 
   return (
-    <main className='relative top-5 md:absolute md:left-20 lg:left-[25%] lg:top-10 h-full md:w-[75%]'>
+    <main className='relative top-5 md:absolute md:left-20 lg:left-[25%] lg:top-10 h-full md:w-[75%] p-5 md:p-0'>
       <h2 className='text-2xl text-cyan-500 font-bold'>Create Product</h2>
 
-      <form className="md:w-[70%] mt-5 flex flex-col gap-5 p-5 md:p-0">
+      <form className="md:w-[70%] mt-5 flex flex-col gap-5">
         {/* row one */}
         <article className='flex flex-col gap-5 md:flex-row justify-between'>
           <div>
